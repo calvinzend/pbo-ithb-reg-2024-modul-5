06bd6be0704e9e65fc36754099966aed335e9420
Magic9904 <calvinzend99@gmail.com>
2024-11-22 17:12:10 +0700

    nama jenis dan tttl

--- src/view/Penduduk.java
+++ src/view/Penduduk.java
@@ -1,57 +1,70 @@
 package view;
 
 import com.toedter.calendar.JDateChooser;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
+import javax.swing.*;
 
 public class Penduduk {
 
     static class Frame extends JFrame {
-        JLabel NIK = new JLabel("NIK  :  ");
-        JTextField NIKValue = new JTextField(10);
-        JLabel nama = new JLabel("NAMA  :  ");
-        JTextField namaValue = new JTextField(10);
-        JLabel tempatL = new JLabel("Tempat Lahir  :  ");
-        JTextField tempatLValue = new JTextField(10);
-        JLabel tanggalLahir = new JLabel("Tanggal Lahir  :  ");
-        private JDateChooser ttlValue = new JDateChooser();
-        
-        JTextArea resultArea = new JTextArea(5, 20);
-
-        JButton submit = new JButton("Submit");
+        CardLayout cardLayout = new CardLayout(); 
+        JPanel cardPanel = new JPanel(cardLayout);
 
         public Frame() {
             super("Input Data Penduduk");
             setSize(600, 400);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             setLayout(new BorderLayout());
+ 
+            JPanel inputPage = createInputPage();
 
-            JPanel p1 = new JPanel(new GridLayout(0, 2, 5, 5));
-            p1.add(NIK);
-            p1.add(NIKValue);
-            p1.add(nama);
-            p1.add(namaValue);
-            p1.add(tempatL);
-            p1.add(tempatLValue);
-            p1.add(tanggalLahir);
-            p1.add(ttlValue);
+            JPanel resultPage = createResultPage();
 
-            resultArea.setEditable(false);
-            JScrollPane scrollPane = new JScrollPane(resultArea);
+            cardPanel.add(inputPage, "InputPage");
+            cardPanel.add(resultPage, "ResultPage");
 
-            add(p1, BorderLayout.NORTH);
-            add(scrollPane, BorderLayout.CENTER);
-            add(submit, BorderLayout.SOUTH);
+            add(cardPanel, BorderLayout.CENTER);
+
+            setVisible(true);
+        }
+
+        private JPanel createInputPage() {
+            JPanel inputPanel = new JPanel(new BorderLayout());
+            JPanel formPanel = new JPanel(new GridLayout(0, 2, 5, 5));
+
+            JLabel NIK = new JLabel(" NIK  :  ");
+            JTextField NIKValue = new JTextField(10);
+            JLabel nama = new JLabel(" NAMA  :  ");
+            JTextField namaValue = new JTextField(10);
+            JLabel tempatL = new JLabel(" Tempat Lahir  :  ");
+            JTextField tempatLValue = new JTextField(10);
+            JLabel tanggalLahir = new JLabel(" Tanggal Lahir  :  ");
+            JDateChooser ttlValue = new JDateChooser();
+            JRadioButton priaButton = new JRadioButton("Pria");
+            JRadioButton wanitaButton = new JRadioButton("Wanita");
+            JLabel jenisKelamin = new JLabel("Pilih Jenis Kelamin:");
+            JLabel kosong =  new JLabel("\n");
+
+            JButton submit = new JButton("Submit");
+
+            formPanel.add(NIK);
+            formPanel.add(NIKValue);
+            formPanel.add(nama);
+            formPanel.add(namaValue);
+            formPanel.add(tempatL);
+            formPanel.add(tempatLValue);
+            formPanel.add(tanggalLahir);
+            formPanel.add(ttlValue);
+            formPanel.add(jenisKelamin);
+            formPanel.add(kosong);
+            formPanel.add(priaButton);
+            formPanel.add(wanitaButton);
+
+            inputPanel.add(formPanel, BorderLayout.CENTER);
+            inputPanel.add(submit, BorderLayout.SOUTH);
 
             submit.addActionListener(new ActionListener() {
                 @Override
@@ -60,29 +73,55 @@ public class Penduduk {
                     String nama = namaValue.getText().trim();
                     String tempat = tempatLValue.getText().trim();
                     java.util.Date tanggal = ttlValue.getDate();
+                    String jenisKelamin = null;
 
-                    if (nik.isEmpty() || nama.isEmpty() || tempat.isEmpty() || tanggal == null) {
-                        resultArea.setText("Mohon isi semua field!\n");
-                        return;
-                    }else{
-                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-                        String tanggalFormatted = sdf.format(tanggal);
-    
-                        resultArea.append("NIK: " + nik + "\n");
-                        resultArea.append("Nama: " + nama + "\n");
-                        resultArea.append("Tempat, Tanggal Lahir: " + tempat + ", " + tanggalFormatted + "\n");
-                        resultArea.append("--------------------------\n");
-                        
+                    if (priaButton.isSelected()) {
+                        jenisKelamin = "Pria";
+                    } else if (wanitaButton.isSelected()) {
+                        jenisKelamin = "Wanita";
                     }
 
-                    NIKValue.setText("");
-                    namaValue.setText("");
-                    tempatLValue.setText("");
-                    ttlValue.setDate(null);
+                    if (nik.isEmpty() || nama.isEmpty() || tempat.isEmpty() || tanggal == null) {
+                        JOptionPane.showMessageDialog(inputPanel, "Mohon isi semua field!", "Error", JOptionPane.ERROR_MESSAGE);
+                        return;
+                    } else {
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                        String tanggalFormatted = sdf.format(tanggal);
+
+                        JLabel resultLabel = (JLabel) ((JPanel) cardPanel.getComponent(1)).getComponent(0);
+                        String hasil = "<html>" +
+                                "<b>NIK:</b> " + nik + "<br>" +
+                                "<b>Nama:</b> " + nama + "<br>" +
+                                "<b>Tempat, Tanggal Lahir:</b> " + tempat + ", " + tanggalFormatted + "<br>" +
+                                "<b>Jenis Kelamin:</b> " + jenisKelamin + "<br>" +
+                                "</html>";
+                        resultLabel.setText(hasil);
+
+                        cardLayout.show(cardPanel, "ResultPage");
+                    }
                 }
             });
 
-            setVisible(true);
+            return inputPanel;
+        }
+
+        private JPanel createResultPage() {
+            JPanel resultPanel = new JPanel(new BorderLayout());
+            JLabel resultLabel = new JLabel("<html><b>Hasil akan ditampilkan di sini</b></html>", SwingConstants.CENTER);
+
+            JButton backButton = new JButton("Kembali");
+
+            resultPanel.add(resultLabel, BorderLayout.CENTER);
+            resultPanel.add(backButton, BorderLayout.SOUTH);
+
+            backButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    cardLayout.show(cardPanel, "InputPage");
+                }
+            });
+
+            return resultPanel;
         }
     }
 
