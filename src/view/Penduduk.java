90711c8d28c606ddd98df609984e7f160ee3e927
Magic9904 <calvinzend99@gmail.com>
2024-11-23 10:43:37 +0700

    udah sampai nomor 10

--- src/view/Penduduk.java
+++ src/view/Penduduk.java
@@ -46,22 +46,66 @@ public class Penduduk {
             JRadioButton priaButton = new JRadioButton("Pria");
             JRadioButton wanitaButton = new JRadioButton("Wanita");
             JLabel jenisKelamin = new JLabel("Pilih Jenis Kelamin:");
+            JLabel golonganDarah = new JLabel("Pilih Gol Darah:");
+            JRadioButton golA = new JRadioButton("A");
+            JRadioButton golB = new JRadioButton("B");
+            JRadioButton golO = new JRadioButton("O");
+            JRadioButton golAB = new JRadioButton("AB");
+
+            JLabel alamat = new JLabel(" Alamat  :  ");
+            JTextField alamatValue = new JTextField(10);
+            JLabel RTRW = new JLabel(" RT/RW  :  ");
+            JTextField RTRWValue = new JTextField(10);
+
+            JLabel KelDesa= new JLabel("Kel/Desa : ");
+            JTextField KelDesaValue = new JTextField(10);
+            JLabel kecamatan= new JLabel("Kecamatan : ");
+            JTextField kecamatanValue = new JTextField(10);
+
+
+
             JLabel kosong =  new JLabel("\n");
+            JLabel kosong2 =  new JLabel("\n");
 
             JButton submit = new JButton("Submit");
 
+            //NIK
             formPanel.add(NIK);
             formPanel.add(NIKValue);
+            //Nama
             formPanel.add(nama);
             formPanel.add(namaValue);
+            //Tempat Tanggal lahir
             formPanel.add(tempatL);
             formPanel.add(tempatLValue);
+            //Tanggal Lahir
             formPanel.add(tanggalLahir);
             formPanel.add(ttlValue);
+            //Jenis Kelamain
             formPanel.add(jenisKelamin);
             formPanel.add(kosong);
             formPanel.add(priaButton);
             formPanel.add(wanitaButton);
+            //Golongan Darah
+            formPanel.add(golonganDarah);
+            formPanel.add(kosong2);
+            formPanel.add(golA);
+            formPanel.add(golB);
+            formPanel.add(golO);
+            formPanel.add(golAB);
+            //Alamat
+            formPanel.add(alamat);
+            formPanel.add(alamatValue);
+            //RT/RW
+            formPanel.add(RTRW);
+            formPanel.add(RTRWValue);
+            //Kel/Desa
+            formPanel.add(KelDesa);
+            formPanel.add(KelDesaValue);
+            //Kecamatan
+            formPanel.add(kecamatan);
+            formPanel.add(kecamatanValue);
+
 
             inputPanel.add(formPanel, BorderLayout.CENTER);
             inputPanel.add(submit, BorderLayout.SOUTH);
@@ -74,12 +118,27 @@ public class Penduduk {
                     String tempat = tempatLValue.getText().trim();
                     java.util.Date tanggal = ttlValue.getDate();
                     String jenisKelamin = null;
+                    String golonganDarah = null;
+                    String alamat = alamatValue.getText();
 
                     if (priaButton.isSelected()) {
-                        jenisKelamin = "Pria";
+                        jenisKelamin = priaButton.getText();
                     } else if (wanitaButton.isSelected()) {
-                        jenisKelamin = "Wanita";
+                        jenisKelamin = wanitaButton.getText();
                     }
+
+                    if (golA.isSelected()) {
+                        golonganDarah = golA.getText();
+                    }else if ( golB.isSelected() ) {
+                        golonganDarah = golB.getText();
+                        
+                    }else if (golO.isSelected()) {
+                        golonganDarah = golO.getText();
+                        
+                    }else if(golAB.isSelected()){
+                        golonganDarah = golAB.getText();
+                    }
+
 
                     if (nik.isEmpty() || nama.isEmpty() || tempat.isEmpty() || tanggal == null) {
                         JOptionPane.showMessageDialog(inputPanel, "Mohon isi semua field!", "Error", JOptionPane.ERROR_MESSAGE);
@@ -94,6 +153,8 @@ public class Penduduk {
                                 "<b>Nama:</b> " + nama + "<br>" +
                                 "<b>Tempat, Tanggal Lahir:</b> " + tempat + ", " + tanggalFormatted + "<br>" +
                                 "<b>Jenis Kelamin:</b> " + jenisKelamin + "<br>" +
+                                "<b>Golongan Darah:</b> " + golonganDarah + "<br>" +
+                                "<b>Alamat:</b> " + alamat + "<br>" +
                                 "</html>";
                         resultLabel.setText(hasil);
 
